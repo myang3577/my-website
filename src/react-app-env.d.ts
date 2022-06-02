2714b52c3d036826f6523d5135b6f031f28521bc
Michael Yang <michael.getemail@gmail.com>
2022-06-01 21:41:51 -0700

    Remove some default code, reformat files

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+/**
+ * From: https://stackoverflow.com/a/67263068
+ *
+ * This file references TypeScript types declarations that are specific to projects started with Create React App.
+ *
+ * These type declarations add support for importing resource files such as bmp, gif, jpeg, jpg, png, webp, and svg. That means that the following import will work as expected without errors:
+ *
+ * import logo from './logo.svg';
+ * It also adds support for importing CSS Modules. This relates to import of files with .module.css,.module.scss, and .module.sass extensions.
+ */
