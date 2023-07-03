b8aa3c7f52f2f7ac74b7a596dc5a162bbf2c15d5
Michael Yang <michael.getemail@gmail.com>
2023-07-02 22:01:50 -0700

    Add action area to augment cards.

--- src/components/teamfighttactics/augments/AugmentCard.tsx
+++ src/components/teamfighttactics/augments/AugmentCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Link, Typography } from "@mui/material";
 
 import { TftMetasrc } from "../../../slices/teamfighttactics/types/TftMetasrc";
 
@@ -6,27 +6,31 @@ interface AugmentCardProps {
   augment: TftMetasrc | undefined;
 }
 
-export const AugmentCard = ({ augment }: AugmentCardProps) => {
-  return (
-    <div>
-      {augment !== undefined && (
-        <Card variant="outlined" sx={{ display: "flex", flexDirection: "column" }}>
-          <CardMedia
-            component="img"
-            sx={{
-              width: "auto",
-              height: "100px",
-              margin: "auto",
-              paddingTop: "5px",
-            }}
-            src={augment.image}
-          />
-          <CardContent sx={{ flex: "1 0 auto" }}>
-            <Typography variant="h6">{augment.name}</Typography>
-            <Typography variant="body2">{augment.tier} Tier</Typography>
-          </CardContent>
-        </Card>
-      )}
-    </div>
-  );
-};
+const METASRC_BASE_URL = "https://www.metasrc.com/tft";
+
+export const AugmentCard = ({ augment }: AugmentCardProps) => (
+  <div>
+    {augment !== undefined && (
+      <Card variant="outlined" sx={{ display: "flex", flexDirection: "column" }}>
+        <CardActionArea>
+          <Link href={`${METASRC_BASE_URL}${augment.path}`} underline="hover" target="_blank">
+            <CardMedia
+              component="img"
+              sx={{
+                width: "auto",
+                height: "100px",
+                margin: "auto",
+                paddingTop: "5px",
+              }}
+              src={augment.image}
+            />
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <Typography variant="h6">{augment.name}</Typography>
+              <Typography variant="body2">{augment.tier} Tier</Typography>
+            </CardContent>
+          </Link>
+        </CardActionArea>
+      </Card>
+    )}
+  </div>
+);
